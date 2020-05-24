d659d5d5a2ebd1032bd9ba26f2b2b5d71c38929b
David Adams <david.adams.16.12@gmail.com>
2020-05-24 21:55:39 +1000

    Add min, max, and average duration stats to report

--- attack.ts
+++ attack.ts
@@ -9,11 +9,9 @@ export const attack = async (url: string, requests: number, duration: number) =>
   console.log('----------------------------------------');
 
   const requestDelay = duration / Math.max(requests - 1, 1);
-  let errors = 0;
   const responses: Promise<any>[] = [];
   for (let i = 0; i < requests; ++i) {
-    const req = axios.get(url).catch(() => { errors++ });
-    console.log('Sent request');
+    const req = requestWithTime(url);
     responses.push(req);
 
     if (i < requests - 1) {
@@ -22,16 +20,36 @@ export const attack = async (url: string, requests: number, duration: number) =>
   }
 
   console.log(`${requests} requests sent`);
-  await Promise.all(responses);
+  const results = await Promise.all(responses);
+  const elapsedTime = (new Date().getTime() - start) / 1000;
+  const errors = results.reduce((acc, { error }) => acc + error, 0);
+  const totalDuration = results.reduce((acc, { duration }) => acc + duration, 0);
+  const averageDuration = totalDuration / requests;
+  const minDuration = Math.min(...results.map(r => r.duration));
+  const maxDuration = Math.max(...results.map(r => r.duration));
+
   console.log(`Attack completed at: ${new Date()}`);
-  console.log(`Attack duration: ${(new Date().getTime() - start) / 1000}`);
+  console.log(`Attack duration: ${elapsedTime}`);
   console.log(`Error responses received: ${errors}`);
+  console.log(`Response time in ms (min/avg/max): ${minDuration}/${averageDuration}/${maxDuration}`);
 }
 
 function sleep (ms: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+}
+
+async function requestWithTime (url: string) {
+  let error = false;
+  const start = new Date().getTime();
+  await axios.get(url).catch(() => error = true);
+  const duration = new Date().getTime() - start;
+
+  return {
+    error,
+    duration,
+  };
 }
 
 if (process.argv.length < 2 + 3) {
